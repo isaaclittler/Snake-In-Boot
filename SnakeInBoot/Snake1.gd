8eb4a1a7b51026b7f51985d303601f66246e1929
bender <3070468+isaac.littler@users.noreply.github.com>
2019-01-18 23:34:56 -0600

    Added base snake movement

---
+++ SnakeInBoot/Snake1.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+const GRAVITY = 200.0
+const BASE_SPEED = 200
+const JUMP_SPEED = 100
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	velocity.y += delta * GRAVITY
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -BASE_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  BASE_SPEED
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = -JUMP_SPEED
+	else:
+		velocity.x = 0
+	move_and_slide(velocity, Vector2(0, -1))
