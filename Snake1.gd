0b7ac48ec74962660461f9ab779917c47b7a6a5c
Nick <nvaughan@iastate.edu>
2019-01-19 15:51:06 -0600

    Snake jump reset added

--- Snake1.gd
+++ Snake1.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const GRAVITY = 200.0
 const BASE_SPEED = 200
-const JUMP_SPEED = 100
+const JUMP_SPEED = 200
 
 var velocity = Vector2()
 
@@ -12,8 +12,13 @@ func _physics_process(delta):
 		velocity.x = -BASE_SPEED
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  BASE_SPEED
-	elif Input.is_action_pressed("ui_up"):
-		velocity.y = -JUMP_SPEED
 	else:
 		velocity.x = 0
+		
+	if (is_on_ceiling()):
+		velocity.y += JUMP_SPEED / 2
+		
+	if Input.is_action_just_pressed("ui_up") && is_on_floor():
+		velocity.y = -JUMP_SPEED
+	
 	move_and_slide(velocity, Vector2(0, -1))
