0af12801cf639ccd85431746a4b6d10b0d8e35cc
bender <3070468+isaac.littler@users.noreply.github.com>
2019-01-19 23:26:34 -0600

    reset level after win condition reached, required adding new main scene that we will instance scenes from in future

--- level1.gd
+++ level1.gd
@@ -4,8 +4,11 @@ extends Node
 var snakes = 0
 var cameraControl
 var finishX = 700
+signal reset
 
 func _ready(): #set the finish point by object in here
+	connect("reset", self, "root")
+	print("level loaded")
 	pass
 
 func winCond():
@@ -14,9 +17,23 @@ func winCond():
 		snakes+=1
 	if snakes== 0:
 		print("bird win")
+		emit_signal("reset")
 	cameraControl = get_child(0)
 	if (cameraControl.furthestX >= finishX):
 		print(cameraControl.winningSnake," snake wins")
+		emit_signal("reset")
+
+func reLoad():
+	# Remove the current level
+	var root = get_owner()
+	var level = root.get_node("level")
+	root.remove_child(level)
+	level.call_deferred("free")
+
+	# Add the next level
+	var next_level_resource = load("res://level1.tscn")
+	var next_level = next_level.instance()
+	root.add_child(next_level)
 
 func _process(delta):
 	winCond()
