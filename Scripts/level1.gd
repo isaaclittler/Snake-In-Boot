0624ab040c0954a6f679c77d918b1a9708dad228
bender <3070468+isaac.littler@users.noreply.github.com>
2019-01-21 18:39:15 -0600

    working controller input assign from code

--- Scripts/level1.gd
+++ Scripts/level1.gd
@@ -5,6 +5,7 @@ var snakes = 0
 var cameraControl
 export var finishX = 3000
 export var winner = "snake wins"
+export var birdNumber = 0
 signal reset
 
 func _ready(): #set the finish point by object in here
@@ -21,8 +22,26 @@ func _ready(): #set the finish point by object in here
 		var loading = preload("res://Scenes/levels/lvl_3.tscn").instance()
 		add_child(loading)
 		print("2")
+	setControllers()
 	print("level loaded")
 	pass
+
+func setControllers():
+	var i = birdNumber + 1
+	var bird = get_child(1)
+	bird.left = str("p",birdNumber,"L")
+	bird.right = str("p",birdNumber,"R") 
+	bird.a = str("p",birdNumber,"A")
+	bird.b = str("p",birdNumber,"B")  
+	bird.x = str("p",birdNumber,"X") 
+	bird.y = str("p",birdNumber,"Y") 
+	for snake in get_tree().get_nodes_in_group("snakes"):
+		snake.leftInput = str("p",i,"L")
+		snake.rightInput =  str("p",i,"R")
+		snake.aInput =  str("p",i,"A")
+		i += 1
+		if i > 3:
+			i = 0
 
 func winCond():
 	snakes = 0
