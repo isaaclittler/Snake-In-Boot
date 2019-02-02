9b1d08ca2d079754c1d4e77e9e6a305efbe53d92
Bender <isaac.littler@gmail.com>
2019-02-02 16:03:22 -0600

    swap gravity

--- Scripts/Snake1.gd
+++ Scripts/Snake1.gd
@@ -18,7 +18,7 @@ onready var bird = get_node("../Bird1")
 var velocity = Vector2()
 var curAirJumps = 1
 var abilityTemp = ability
-
+var flip = false
 
 
 func _process(delta):
@@ -28,43 +28,114 @@ func _process(delta):
 	updateSprite()
 
 func _physics_process(delta):
-	if ability == 7\
-	and is_on_floor() != true:
-		velocity.y += delta * GRAVITY * .5
-	elif is_on_floor() != true:
-		velocity.y += delta * GRAVITY
-	if Input.is_action_pressed(leftInput)\
-	and ability == 5\
-	and is_on_floor():
-		velocity.x = -(BASE_SPEED + 100) 
-	elif Input.is_action_pressed(rightInput)\
-	and ability == 5\
-	and is_on_floor():
-		velocity.x =  BASE_SPEED + 100
-	elif Input.is_action_pressed(leftInput):
-		velocity.x = -BASE_SPEED
+	gravity(delta)
+	if Input.is_action_pressed(leftInput):
+		move(-1)
 	elif Input.is_action_pressed(rightInput):
-		velocity.x =  BASE_SPEED
+		move(1)
 	else:
-		velocity.x = 0
-	if (is_on_ceiling()):
-		velocity.y += JUMP_SPEED / 2
-	if is_on_floor():
-		curAirJumps = numAirJumps
-	if Input.is_action_just_pressed(aInput) and is_on_floor():
-		velocity.y = -JUMP_SPEED
-	elif Input.is_action_just_pressed(aInput) and curAirJumps != 0:
-		velocity.y = -JUMP_SPEED
-		curAirJumps -= 1
+		move(0)
+	floorCeiling()
+	if Input.is_action_just_pressed(aInput):
+		if is_on_floor()\
+		and flip == false:
+			jump()
+			print("ground jump")
+		if is_on_ceiling()\
+		and flip == true:
+			jump()
+			print("ground jump")
+	if Input.is_action_just_pressed(aInput) and curAirJumps != 0:
+		if is_on_floor() == false\
+		and flip == false:
+			jump()
+			curAirJumps -= 1
+			print("air jump")
+		if is_on_ceiling() == false\
+		and flip == true:
+			jump()
+			curAirJumps -= 1
+			print("air jump")
 	elif Input.is_action_just_pressed(aInput)\
 	and ability == 1\
 	and curAirJumps == 0:
-		velocity.y = -JUMP_SPEED
+		jump()
 		ability = 0
 		print("extra jump")
 	if Input.is_action_just_pressed(bInput):
 		useAbility()
-	move_and_slide(velocity, Vector2(0, -1))
+	if flip == false:
+		move_and_slide(velocity, Vector2(0, -1))
+	else:
+		move_and_slide(velocity, Vector2(1, 0))
+
+func floorCeiling():
+	if (is_on_ceiling()):
+		if flip == false:
+			velocity.y += JUMP_SPEED / 2
+		else:
+			curAirJumps = numAirJumps
+	if is_on_floor():
+		if flip == false:
+			curAirJumps = numAirJumps
+		else:
+			velocity.x += JUMP_SPEED / 2
+
+func gravity(delta):
+	if flip == false:
+		if ability == 7\
+		and is_on_floor() != true\
+		and velocity.y < GRAVITY * .5:
+			velocity.y += delta * GRAVITY * .5
+		elif is_on_floor() != true\
+		and velocity.y < GRAVITY:
+			velocity.y += delta * GRAVITY
+	else:
+		if ability == 7\
+		and is_on_floor() != true\
+		and velocity.x < GRAVITY * .5:
+			velocity.x += delta * GRAVITY * .5
+		elif is_on_ceiling() != true\
+		and velocity.x < GRAVITY:
+			velocity.x += delta * GRAVITY
+
+func jump():
+	if flip == false:
+		velocity.y = -JUMP_SPEED
+	else:
+		velocity.x = -JUMP_SPEED
+
+func move(direction):
+	var speed = BASE_SPEED
+	if ability == 5:
+		if is_on_floor() == true\
+		and flip == false:
+			speed += 100
+		if is_on_ceiling() == true\
+		and flip == true:
+			speed += 100
+	if direction == 1: #right
+		if flip == false:
+			velocity.x = speed
+		else:
+			velocity.y = -speed
+	elif direction == -1: #left
+		if flip == false:
+			velocity.x = -speed
+		else:
+			velocity.y = speed
+	else:
+		if flip == false:
+			velocity.x = 0
+		else:
+			velocity.y = 0
+
+func flipGravity():
+	var temp = velocity.x
+	velocity.x = velocity.y
+	velocity.y = temp
+	print(velocity)
+	move_and_slide(velocity, Vector2(1,0))
 
 func useAbility():
 	if ability == 0:
@@ -78,6 +149,11 @@ func useAbility():
 		platform.position = Vector2(self.position.x + 32,self.position.y + 30)
 		get_parent().add_child(platform)
 		ability = 0
+	if ability == 4:
+		self.rotation = -(.5 * PI)
+		flip = true
+		velocity.x = 0
+		velocity.y = 0
 
 func updateSprite():
 	var sprite = get_child(0)
--- Scripts/level1.gd
+++ Scripts/level1.gd
@@ -35,13 +35,34 @@ func load_boss():
 
 func load_snakes():
 	var i = 0
+	var temp
 	var loading
 	for i in (numPlayers - 1):
 		loading = load("res://Scenes/characters/Snake1.tscn").instance()
 		add_child(loading)
-		loading.position.x += i * 32
-		loading.spriteY += i * 32
-		print(loading.spriteY)
+		temp = i * 32
+		loading.position.x = temp * 1.5 - 32
+		#loading.spriteY += temp
+		print(loading.position.x)
+
+func bossAssign(i):
+	var bird = get_child(2)
+	var birdNumber = i
+	if bossNumber == 0:
+		bird.left = str("p",birdNumber,"L")
+		bird.right = str("p",birdNumber,"R") 
+		bird.a = str("p",birdNumber,"A")
+		bird.b = str("p",birdNumber,"B")  
+		bird.x = str("p",birdNumber,"X") 
+		bird.y = str("p",birdNumber,"Y") 
+
+func snakeAssign(controllerNum,snakeNum):
+	var snake = get_tree().get_nodes_in_group("snakes")[snakeNum]
+	snake.leftInput = str("p",controllerNum,"L")
+	snake.rightInput =  str("p",controllerNum,"R")
+	snake.aInput =  str("p",controllerNum,"A")
+	snake.bInput =  str("p",controllerNum,"B")
+	snake.spriteY = controllerNum * 32 + 1
 
 func setControllers():
 	var snakeAbi = get_parent().snakeAbilities
@@ -52,29 +73,30 @@ func setControllers():
 		birdNumber = get_parent().playerWinner
 	print("bird,",birdNumber)
 	var i = birdNumber + 1
-	var bird = get_child(2)
-	bird.left = str("p",birdNumber,"L")
-	bird.right = str("p",birdNumber,"R") 
-	bird.a = str("p",birdNumber,"A")
-	bird.b = str("p",birdNumber,"B")  
-	bird.x = str("p",birdNumber,"X") 
-	bird.y = str("p",birdNumber,"Y") 
-	for snake in get_tree().get_nodes_in_group("snakes"):
-		if snakeAbi > 0:
-			if snakeAbi > 3:
-				snakeAbi = 3
-			var ability = randi()%4 + 1
-			if ability == 4:
-				ability = 5
-			snake.ability = ability
-			snakeAbi += -1
-		snake.leftInput = str("p",i,"L")
-		snake.rightInput =  str("p",i,"R")
-		snake.aInput =  str("p",i,"A")
-		snake.bInput =  str("p",i,"B")
-		i += 1
-		if i > 3: #trying 4, was 3
-			i = 0
+	var k = 0
+	var numSnakes = 0
+	for k in 4:
+		if k == birdNumber:
+			bossAssign(k)
+		else:
+			snakeAssign(k,numSnakes)
+			numSnakes += 1
+#	for snake in get_tree().get_nodes_in_group("snakes"):
+#		if snakeAbi > 0:
+#			if snakeAbi > 3:
+#				snakeAbi = 3
+#			var ability = randi()%4 + 1
+#			if ability == 4:
+#				ability = 5
+#			snake.ability = ability
+#			snakeAbi += -1
+#		snake.leftInput = str("p",i,"L")
+#		snake.rightInput =  str("p",i,"R")
+#		snake.aInput =  str("p",i,"A")
+#		snake.bInput =  str("p",i,"B")
+#		i += 1
+#		if i > 3: #trying 4, was 3
+#			i = 0
 
 func winCond():
 	snakes = 0
